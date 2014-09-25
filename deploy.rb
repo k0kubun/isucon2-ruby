d42d9d9481e49dc7a4c033d7e45ccfa8c0fd59f5
Takashi Kokubun <takashikkbn@gmail.com>
2014-09-26 08:41:49 +0900

    Log deployements

--- deploy.rb
+++ deploy.rb
@@ -6,3 +6,7 @@ include_recipe "./recipes/app.rb"
 include_recipe "./recipes/bench.rb"
 include_recipe "./recipes/supervisord.rb"
 include_recipe "./recipes/nginx.rb"
+
+execute "log deployment" do
+  command "cd /home/isu-user/isucon2-ruby/app; RACK_ENV=production /home/isu-user/isucon2-ruby/config/env.sh newrelic deployment"
+end
