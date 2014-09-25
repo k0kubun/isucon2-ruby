aebcab441d8df2653b47f7dbdd2699b2d43a3056
Takashi Kokubun <takashikkbn@gmail.com>
2014-09-26 00:55:21 +0900

    Bundle install

--- recipes/app.rb
+++ recipes/app.rb
@@ -2,3 +2,5 @@ git "/home/isu-user/isucon2-ruby" do
   repository "git@github.com:k0kubun/isucon2-ruby"
   user "isu-user"
 end
+
+execute "cd /home/isu-user/isucon2-ruby/app; /home/isu-user/isucon2-ruby/config/env.sh bundle install"
