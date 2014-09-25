4169f67817c7ab3acff31233c8fb77e67bd3b241
Takashi Kokubun <takashikkbn@gmail.com>
2014-09-26 01:05:27 +0900

    Deploy by supervisord

--- recipes/supervisord.rb
+++ recipes/supervisord.rb
@@ -1,4 +1,13 @@
 template "/etc/supervisord.conf" do
   action :create
   source "../config/supervisord.conf.erb"
+  notifies :run, "execute[sudo service supervisord restart]"
+end
+
+execute "sudo service supervisord restart" do
+  action :nothing
+end
+
+service "supervisord" do
+  action [:enable, :start]
 end
