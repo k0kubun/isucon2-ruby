80c70d1da233dd1747989c37468e0dd17f8cf144
Takashi Kokubun <takashikkbn@gmail.com>
2014-09-26 20:55:26 +0900

    Fix fatal bug in nginx recipe

--- recipes/nginx.rb
+++ recipes/nginx.rb
@@ -2,9 +2,14 @@ package "nginx" do
   action :install
 end
 
+execute "service nginx restart" do
+  action :nothing
+end
+
 template "/etc/nginx/nginx.conf" do
   action :create
   source "../config/nginx.conf.erb"
+  notifies :run, "execute[service nginx restart]"
 end
 
 service "nginx" do
